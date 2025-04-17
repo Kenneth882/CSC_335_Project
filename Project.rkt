88eac2e1e2d3ee786f6f4652d1536e8361cd01d0
alexisj890 <alexjuarez4002@gmail.com>
2025-04-16 21:46:21 -0400

    some proofs i am still working on them

--- Project.rkt
+++ Project.rkt
@@ -1,7 +1,6 @@
 ;Members: Alexis Juarez, Hamim Choudhury, Kenneth Romero
-;4/9/25
 ;TLS Project
-;Professor Troeger. 
+;Professor Troeger
 
 
 
@@ -90,12 +89,29 @@
 
 
 ; This is used to build the lists, ensuring the specifications of a "hashmap" where each list has a key value pair
-; and the lenght of both of the lists is equal
+; and the length of both of the lists is equal
 
 
 
 ;COMMENTS FOR US: Put any ideas or what you plan to work on/improve or if stuck on something.
 ;; ============================================================================
+; 4/14/25 - Hamim (Comments Below)
+; So 1.1 of the project is telling us to make the functions in chapter 10 of the book ourselves, in R5RS scheme. The functions that they have
+; are
+; 1) lookup-in-entry (this takes two arguments, name and entry)
+; 2) lookup-in-entry-helper (this is the helper function for #1. This is used when name is not found in the first list of an entry)
+; 3) extend-table (this is like cons)
+; 4) lookup-in-table (takes three arguments, name, table, and table-f)
+; 5) expression-to-action (this produces the correct function for each possible S-expression. action == function)
+; 6) atom-to-action
+; 7) list-to-action
+; 8-16) value, meaning, *const, *quote, *identifier, *lambda, *application, *evcon, *else
+; 17-22) primitive?, non-primitive?, apply, apply-primitive, atom?, apply-closure
+
+; Now how do we approach this? In chapter 10 of the book we have some functions written completely and we just need to convert that to R5RS,
+; and some do not. Kenneth already began to create the basic helper functions and write the functions in Scheme (4/14/25). We started off by
+; first creating the basic helper functions, the ones that are in the chapter and commonly used in TLS and then we went through the list one
+; by one and made the functions. 
 
 
 
@@ -103,37 +119,132 @@
 
 
 
-
-;BASIC HELPER FUNCTIONS
+; BASIC HELPER FUNCTIONS. These are basic functions such as atom?, add1, etc.
+; Or they are functions that have been translated to R5RS Scheme.
 ;; ============================================================================
 
-; This is the basic atom function which will check when an element is an atom and saves us from redundent code
-( define ( atom? x)
-     (and (not (pair? x)) (not (null? x))))
+; This is the atom function which will check when an element is an atom and saves us from
+; using redundant code. TLS also assumes this function is built in.
+(define (atom? x)
+  (and (not (pair? x)) (not (null? x))))
 
-;This is a simple helper function that can be callled that counts the number of lists
-(define (count lst)
+
+;This is a simple helper function that can be called that counts the number of elements in a list
+(define (count-elements lst)
   (if (null? lst)
       0
       (+ 1 (count (cdr lst)))))
 
+
+;A function used to add by 1
+(define (add1 n)
+  (+ n 1))
+
+
+;A function used to subtract by 1
+(define (sub1 n)
+  (- n 1))
+
+
 ;IN TLS first refers to car
-( define first car)
+(define first car)
+
 
 ;IN TLS second refers to cadr
 (define second cadr)
 
-(define third  caddr)
+
+;IN TLS third refers to caddr
+(define third caddr)
+
 
 ;IN TLS extend-table refers to cons
-( define extend-table cons)
+(define extend-table cons)
 
-;short program used to add1
-(define (add1 n)
-  (+ n 1))
-; short program used to subtract by 1
-(define( sub1 n)
-  (- n 1))
+
+; There are certain functions that we do not need to make as they are already in Scheme. For example,
+; append, member?, and pair?.
+
+
+
+
+
+
+;; ===========================================================================
+; This is the lookup-in-entry function. Accompanied with it is the lookup-in-entry-helper.
+;; ===========================================================================
+
+
+;pre/specs: takes three arguments, name (what we are looking for), entry (list of names and list of values),
+;and entry-f, an error function if name not found.
+(define (lookup-in-entry name entry entry-f)
+  (lookup-in-entry-helper name
+                        (first entry)
+                        (second entry)
+                        entry-f
+                        ))
+;post: returns the value associated with name if it exists in entry. If it does not exist, calls the entry-f function.
+
+;design idea:
+;when looking up the entry there will be 3 possible cases, one where the name is found in entry, it will then return the associated value with the name.
+;If it does not exist it will return the associated value once the entry-f is called.
+;The third case will be if no name is given aka an empty char/string, then we call the entry-f function.
+; ___________________
+;[_______ayp|nyp_____]------>(first entry)= ayp----->(second entry) =nyp -------->(if ayp and nyp = name then we return entry-f)
+;                                                                                   (needs helper)
+
+;ayp: the first tables (ill go more in detail later i gotta push)
+;nyp: the remaining tables
+
+
+(define (lookup-in-entry-helper name names values entry-f)
+   (cond
+     ((null? names)(entry-f name))
+     ((eq? (car names) name) (car values))
+     (else
+      (lookup-in-entry-helper
+       name
+       (cdr names)
+       (cdr values)
+       entry-f
+       ))))
+
+;Testing the function
+ (define entry '((x y z) (1 2 3)))
+ (lookup-in-entry 'x entry (lambda (name) 'not-found-in-entry)) ;returns 1
+ (lookup-in-entry 'a entry (lambda (name) 'not-found-in-entry)) ;returns not-found-in-entry
+ (lookup-in-entry 'entree '((appetizer entree beverage) (ar luka lebron)) (lambda (name) 'not-found)) ;returns luka
+
+
+
+
+
+;; ===========================================================================
+; This is the lookup-in-table function. 
+;; ===========================================================================
+;pre/specs: this takes three arguments, name (what we are looking for), table (it is a list of entries),
+; and table-f (an error function if name is not found)
+(define (lookup-in-table name table table-f)
+   (cond
+     ((null? table)(table-f name))
+     (else
+      (lookup-in-entry name
+                       (car table)
+                       (lambda (name)
+                         (lookup-in-table name
+                                          (cdr table)
+                                           table-f))))))
+;post: returns the value associated with name if it is found in table. if it does not exist, calls the table-f function.
+
+;Testing the function
+; (define table '(((entree dessert)
+;                (spaghetti spumoni))
+;               ((appetizer entree beverage)
+;                (food tastes good))))
+; (lookup-in-table 'entree table (lambda (name) 'not-found)) ;returns spaghetti
+; (lookup-in-table 'dessert table (lambda (name) 'not-found)) ;returns spumoni
+; (lookup-in-table 'appetizer table (lambda (name) 'not-found)) ;returns food
+; (lookup-in-table 'snacks table (lambda (name) 'not-found)) ;returns not-found
 
 
 
@@ -142,7 +253,7 @@
 
 
 
-;TLS FUNCTIONS
+; TLS FUNCTIONS
 ;; ============================================================================
 
 ;This is logic
@@ -158,7 +269,7 @@
 
 
 
-( define new-entry build-entry)
+(define new-entry build-entry)
 ; we probably need to add somehting that adds the input to it 
 
 ;This function will tell us if a list is a set or if its not.
@@ -183,42 +294,6 @@
 
 
 
-;LOOKUP FUNCTIONs
-;; ============================================================================
-(define (lookup-in-entry name entry entry-f)
-  (lookup-in-entry-helper name
-                        (first entry)
-                        (second entry)
-                        entry-f
-                        ))
-
-;This is supposed to check if the name is in the values
-( define (lookup-in-entry-helper name names values entry-f)
-   (cond
-     ((null? names)(entry-f name))
-     ((eq?(car names) name)
-      (car values))
-     (else
-      (lookup-in-entry-helper
-       name
-       (cdr names)
-       (cdr values)
-       entry-f
-       ))))
-
-;;;This is for table lookup
-
-( define (lookup-in-table name table table-f)
-   ( cond
-      ((null? table)(table-f name))
-      (else
-       (lookup-in-entry name
-                        (car table)
-                        (lambda(name)
-                          (lookup-in-table name
-                                           (cdr table)
-                                           table-f))))))
-
 
   
    
@@ -229,9 +304,7 @@
 
 
           
-;Tests
-;(lookup-in-entry 'wine '(appetizer entrée beverage) '(beer beer beer))
-;(lookup-in-entry 'beverage '(appetizer entrée beverage) '(beer beer beer))
+
 
 
 
@@ -468,17 +541,3 @@
 ;(check-set '( 2 3 4 5 5))
 
 ;( count '( 1 3 4 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
