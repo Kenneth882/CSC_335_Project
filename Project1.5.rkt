ecc7589f447fc82bc969ea84b80d5882799b03dc
Hamim <hamimc232@gmail.com>
2025-05-12 22:16:16 -0400

    Added proofs for primitives and lambda

--- Project1.5.rkt
+++ Project1.5.rkt
@@ -31,7 +31,7 @@ functional behavior and TLS-specific constraints.
 and we want these to return the same result as R5RS Scheme.
 
 2) Functions. For lambda functions, TLS represents them as closures (formal parameters, body, environment) and we want to make sure that there
-is proper binding for the parameters. We want to ensure lexical scoping. And ultimately the correct evaluation of the body. It should respect the
+is proper binding for the parameters. We want to ensure lexical scoping. And ultimately the correct evaluation of the body.
 
 3) Errors. In TLS, we are introduced to things such as build-f, entry-f, etc, etc. These are just error messages that are commonly for undefined,
 incorrect number of arguments, primitive errors. And TLS widely says the error.
@@ -45,27 +45,86 @@ booleans should return themselves.
 |#
 
 
+(define atom-to-action
+  (lambda (e)
+    (cond
+      ((number? e) *const)
+      ((eq? e #t) *const)
+      ((eq? e #f) *const)
+      ((eq? e 'cons) *const)
+      ((eq? e 'car) *const)
+      ((eq? e 'cdr) *const)
+      ((eq? e 'null?) *const)
+      ((eq? e 'eq?) *const)
+      ((eq? e 'atom?) *const)
+      ((eq? e 'zero?) *const)
+      ((eq? e 'add1) *const)
+      ((eq? e 'sub1) *const)
+      ((eq? e 'number?) *const)
+      ((eq? e '+) *const)
+      ((eq? e '-) *const)
+      ((eq? e '*) *const)
+      ((eq? e '/) *const)
+      (else *identifier))))
 
 
-;; TLS s-expressions can be:
-;;   - Constants: numbers, booleans
-;;   - Symbols: variables
-;;   - Quoted expressions
-;;   - Lambda expressions
-;;   - Conditionals (cond), (if), (else)
-
-(define (value e)
-  (meaning e '()))
-
-(define (meaning e table)
-  ((expression-to-action e) e table))
+(define (list-to-action e)
+  (cond
+    ((atom? (car e))
+     (cond
+       ((eq? (car e) 'quote)  *quote)
+       ((eq? (car e) 'lambda) *lambda)
+       ((eq? (car e) 'cond)   *cond)
+       (else *application)))
+    (else *application)))
 
 (define (expression-to-action e)
   (if (atom? e)
       (atom-to-action e)
       (list-to-action e)))
-
 ;; expression-to-action determines which case to apply based on syntax.
+
+
+
+(define (meaning e table)
+  ((expression-to-action e) e table))
+
+(define (value e)
+  (meaning e '()))
+
+
+
+;; ===================================================================================================================
+;; PROVING THAT THE IMPLEMENTATION OF OUR TLS INTERPRETER MEETS THE STANDARD
+
+;Let's focus on number 1, primitives. In TLS, cons, car, cdr, null, eq?, etc, etc are constants. We know this because in the atom-to-action
+;function listed above, they're *const. Each primitive should be applied correctly. 
+
+; (atom-to-action 'cons) ;#<procedure:*const>
+; (atom-to-action 'eq?)  ;#<procedure:*const>
+; (atom-to-action 'add1) ;#<procedure:*const>
+
+; (value '(add1 9))          ;10
+; (value '(sub1 8))          ;7
+; (value '(number? 1))       ;#t
+; (value '(number? 'a))      ;#f
+; (value '(cons 'a '(b c)))  ;(a b c)
+; Each result matches what we'd expect in TLS and R5RS Scheme for these primitives. This proves that our interpreter correctly recognizes
+; these primitive symbols and applies them correctly. Keep in mind that for TLS, we don't have +, *, -, /, we need to make things like add1, sub1.
+
+
+
+;Now let's focus on number 2, functions. In TLS, functions can be specified in a special way. Lambda expressions define functions but they are
+;represented as closures. A closure has formal parameters, the body, and the environment. So for us we use expression-to-action to find a lambda
+;function because it uses list-to-action too. And then by *lambda, we make the closure. And *application, applies the closure by evaluating the
+;arguments, binding, and then evaluating the body. This is a complex process, so we need to make sure that it actually returns the correct answer.
+; (value '((lambda (x) x) 1))                                ;1
+; (value '((lambda (x) (add1 x)) 99))                        ;100
+; (value '(((lambda (x) (lambda (y) (cons x y))) 'a) 'b))    ;(a . b)
+; (value '((lambda (x y) (cons x (cons y '()))) 'a 'b))      ;(a b)
+
+
+
 
 #|
 So let's start off with the base step of the interpreter.
